6d9026c003c2a5157576f8a7654dfc82479ac5bd
Twijn <twijn@twijn.net>
2020-05-01 23:36:23 -0500

    Add command executors and listener events

--- src/net/valoranmc/essentials/VMCEssentials.java
+++ src/net/valoranmc/essentials/VMCEssentials.java
@@ -9,12 +9,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import net.valoranmc.essentials.commands.FlyCommand;
 import net.valoranmc.essentials.commands.NickCommand;
+import net.valoranmc.essentials.commands.gamemode.GMACommand;
+import net.valoranmc.essentials.commands.gamemode.GMCCommand;
+import net.valoranmc.essentials.commands.gamemode.GMSCommand;
+import net.valoranmc.essentials.commands.gamemode.GMSPCommand;
+import net.valoranmc.essentials.commands.gamemode.GamemodeCommand;
 import net.valoranmc.essentials.commands.tpa.TPACommand;
 import net.valoranmc.essentials.commands.tpa.TPAHereCommand;
 import net.valoranmc.essentials.commands.tpa.TPAcceptCommand;
 import net.valoranmc.essentials.commands.tpa.TPDenyCommand;
 import net.valoranmc.essentials.commands.tpo.TPOCommand;
 import net.valoranmc.essentials.commands.tpo.TPOHereCommand;
+import net.valoranmc.essentials.listeners.DisableMobListener;
+import net.valoranmc.essentials.listeners.FlyPersistenceListener;
 import net.valoranmc.essentials.listeners.NickListener;
 import net.valoranmc.essentials.listeners.TPARequestListener;
 import net.valoranmc.essentials.utils.DBUtils;
@@ -39,10 +46,21 @@ public class VMCEssentials extends JavaPlugin {
 		getCommand("tpahere").setExecutor(new TPAHereCommand());
 		getCommand("tpaccept").setExecutor(new TPAcceptCommand());
 		getCommand("tpdeny").setExecutor(new TPDenyCommand());
+
+		getCommand("gma").setExecutor(new GMACommand());
+		getCommand("gmc").setExecutor(new GMCCommand());
+		getCommand("gms").setExecutor(new GMSCommand());
+		getCommand("gmsp").setExecutor(new GMSPCommand());
+		
+		GamemodeCommand gmCommand = new GamemodeCommand();
+		getCommand("gamemode").setExecutor(gmCommand);
+		getCommand("gamemode").setTabCompleter(gmCommand);
 		
 		PluginManager pm = Bukkit.getPluginManager();
 		pm.registerEvents(new NickListener(), this);
 		pm.registerEvents(new TPARequestListener(), this);
+		pm.registerEvents(new DisableMobListener(), this);
+		pm.registerEvents(new FlyPersistenceListener(), this);
 		
 		Connection con = null;
 		try {
